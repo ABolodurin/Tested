a851d2178b9cc7c7cc328b4d2b1366d3741bf986
Alex Bolodurin <shur026@yandex.ru>
2022-10-25 13:26:48 +0300

    1.0

---
+++ GitControlled/src/Lesson/Lesson.java
@@ -0,0 +1,10 @@
+package Lesson;
+
+public class Lesson {
+    public static void main(String[] args) {
+        message();
+    }
+    private static void message(){
+        System.out.println("message");
+    }
+}
